04e9684259f39c7dc242f45c79ee8216b0cdc1b8
heyheman11 <daniel.zambetto@gmail.com>
2021-02-09 19:37:42 +1100

    improvements

--- src/hooks.js
+++ src/hooks.js
@@ -22,4 +22,45 @@ function useWindowDimensions() {
   return [width, height];
 }
 
-export { useWindowDimensions };
+const useDimensions = (ref) => {
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const copiedRef = ref;
+    const resize = new ResizeObserver((node) => {
+      setHeight(node[0]?.contentRect?.height);
+      setWidth(node[0]?.contentRect?.width);
+    });
+    if (copiedRef && "current" in copiedRef && copiedRef.current !== null) {
+      resize.observe(copiedRef.current);
+    }
+    return () => {
+      if (copiedRef && "current" in copiedRef && copiedRef.current !== null) {
+        resize.unobserve(copiedRef?.current);
+      }
+    };
+  }, [ref]);
+  return [width, height];
+};
+
+const useMouse = (ref) => {
+  const [coord, setCoord] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const copiedRef = ref;
+    if (copiedRef && "current" in copiedRef && copiedRef.current !== null) {
+      copiedRef.current.addEventListener("mousemove", (event) => {
+        setCoord({ x: event.clientX, y: event.clientY });
+      });
+    }
+    return () => {
+      if (copiedRef && "current" in copiedRef && copiedRef.current !== null) {
+        // copiedRef.current.addEventListener;
+      }
+    };
+  }, [ref]);
+  return coord;
+};
+
+export { useWindowDimensions, useDimensions, useMouse };
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,12 +1,32 @@
+import { useRef, useEffect } from "react";
 import "./About.css";
+import { useDimensions, useMouse } from "../../hooks";
+
+const ROTATE_CONSTANT = 10;
 
 export function About() {
+  const pageRef = useRef(null);
+  const [width, height] = useDimensions(pageRef);
+  const { x, y } = useMouse(pageRef);
+
+  console.log("x: ", x, "y: ", y);
+
+  useEffect(() => {
+    const xCoo = (x / (width / 2)) * ROTATE_CONSTANT - ROTATE_CONSTANT;
+    const yCoo =
+      (y / (height / 2)) * ROTATE_CONSTANT - ROTATE_CONSTANT;
+    if (pageRef && "current" in pageRef) {
+      pageRef.current.style.setProperty("--rotate-x", `${xCoo}deg`);
+      pageRef.current.style.setProperty("--rotate-y", `${yCoo * -1}deg`);
+    }
+  }, [x, y, width, height]);
+
   return (
-    <div className="about-page">
+    <div className="about-page" ref={pageRef}>
       <div className="area">
         <div className={"cube"}>
           <div className="cube-face-front cube-face--front">
-            Daniel Zambetto Front
+            Daniel Zambetto
           </div>
           <div className="cube-face-front cube-face--back"></div>
           <div className="cube-face-side cube-face--right"></div>
