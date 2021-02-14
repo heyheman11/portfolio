c46e42306ca69703cb3271d2e508fdc80dab300e
heyheman11 <daniel.zambetto@gmail.com>
2021-02-14 17:44:08 +1100

    changes and improvements

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -9,12 +9,9 @@ export function About() {
   const [width, height] = useDimensions(pageRef);
   const { x, y } = useMouse(pageRef);
 
-  console.log("x: ", x, "y: ", y);
-
   useEffect(() => {
     const xCoo = (x / (width / 2)) * ROTATE_CONSTANT - ROTATE_CONSTANT;
-    const yCoo =
-      (y / (height / 2)) * ROTATE_CONSTANT - ROTATE_CONSTANT;
+    const yCoo = (y / (height / 2)) * ROTATE_CONSTANT - ROTATE_CONSTANT;
     if (pageRef && "current" in pageRef) {
       pageRef.current.style.setProperty("--rotate-x", `${xCoo}deg`);
       pageRef.current.style.setProperty("--rotate-y", `${yCoo * -1}deg`);
@@ -25,8 +22,18 @@ export function About() {
     <div className="about-page" ref={pageRef}>
       <div className="area">
         <div className={"cube"}>
+          <div className="orbit earth">
+            <div className="inner-orbit">
+              <div className="child">🌏</div>
+            </div>
+          </div>
+          <div className="orbit saturn">
+            <div className="inner-orbit">
+              <div className="child">🪐</div>
+            </div>
+          </div>
           <div className="cube-face-front cube-face--front">
-            Daniel Zambetto
+            <h3>Daniel Zambetto</h3>
           </div>
           <div className="cube-face-front cube-face--back"></div>
           <div className="cube-face-side cube-face--right"></div>
