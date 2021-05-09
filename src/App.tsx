8b93f80a889883da5c5587ac9ab890ff1e782e0b
heyheman11 <daniel.zambetto@gmail.com>
2021-05-09 21:01:46 +1000

    resume only

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useMemo, useRef, useState, useCallback, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
+// import { useMemo, useRef, useState, useCallback, useEffect } from "react";
 import { Body } from "./body/Body";
-import { Nav } from "./nav/Nav";
+// import { Nav } from "./nav/Nav";
 import routes from "./routes";
 import "./App.css";
 import { useWindowDimensions } from "./hooks";
@@ -8,27 +9,27 @@ import { useWindowDimensions } from "./hooks";
 const MOBILE_WIDTH_BREAKPOINT = 800;
 
 function App() {
-  const [selected, setSelected] = useState(routes[0].name);
+  // const [selected, setSelected] = useState(routes[0].name);
   const [width] = useWindowDimensions();
   const pageRef = useRef(null);
   const [layout, setLayout] = useState<"desktop" | "mobile">("desktop");
   const [isMobileNavActive, setIsMobileNavActive] = useState(false);
 
-  const navRoutes = useMemo(() => {
-    return routes.map((route) => route.name);
-  }, []);
+  // const navRoutes = useMemo(() => {
+  //   return routes.map((route) => route.name);
+  // }, []);
 
-  const setPageSelection = useCallback((page: "About" | "Resume") => {
-    if (page === "About") {
-      document.documentElement.style.setProperty("--bg-colour-1", "#536976");
-      document.documentElement.style.setProperty("--bg-colour-2", "#292e49");
-    }
-    if (page === "Resume") {
-      document.documentElement.style.setProperty("--bg-colour-1", "#ab3827");
-      document.documentElement.style.setProperty("--bg-colour-2", "#bbcf26");
-    }
-    setSelected(page);
-  }, []);
+  // const setPageSelection = useCallback((page: "About" | "Resume") => {
+  //   if (page === "About") {
+  //     document.documentElement.style.setProperty("--bg-colour-1", "#536976");
+  //     document.documentElement.style.setProperty("--bg-colour-2", "#292e49");
+  //   }
+  //   if (page === "Resume") {
+  //     document.documentElement.style.setProperty("--bg-colour-1", "#ab3827");
+  //     document.documentElement.style.setProperty("--bg-colour-2", "#bbcf26");
+  //   }
+  //   setSelected(page);
+  // }, []);
 
   useEffect(() => {
     if (width <= MOBILE_WIDTH_BREAKPOINT) {
@@ -47,7 +48,7 @@ function App() {
 
   return (
     <div className="page" ref={pageRef}>
-      {layout === "mobile" && isMobileNavActive && (
+      {/* {layout === "mobile" && isMobileNavActive && (
         <div className="mobile-nav-wrapper">
           <Nav
             pageRef={pageRef}
@@ -63,10 +64,10 @@ function App() {
           routes={navRoutes}
           setSelected={setPageSelection}
         />
-      )}
+      )} */}
       <Body
         pageRef={pageRef}
-        PageToRender={routes.find((item) => item.name === selected)?.component}
+        PageToRender={routes[1].component}
         layout={layout}
         handleMobileNavToggle={handleMobileNavToggle}
       />
--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -1,20 +1,23 @@
+import { Fragment } from "react";
 import "./Resume.css";
 import resumeData from "./resume-data.json";
 
 function Heading({ title }: any) {
   return (
     <div className="heading">
-      <h2>{title}</h2>
+      <h2 className="gradient-text">{title}</h2>
+      <div className="bottom-border"></div>
     </div>
   );
 }
 
-function Section({ section }: any) {
+function List({ items }: { items: Array<string> }) {
   return (
-    <div>
-      <Heading title={section.title} />
-      <p>{section.description}</p>
-    </div>
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
   );
 }
 
@@ -26,12 +29,12 @@ function ExperienceNode({ item }: any) {
       </div>
       <div className="workplace">
         <h3>{item.role}</h3>
-        <p>
-          {item.company}
-          {" / "}
-          <span className="date">{item.date}</span>
-        </p>
-        <p>{item.description}</p>
+        <DateStamp title={item.company} date={item.date} />
+        {Array.isArray(item.description) ? (
+          <List items={item.description} />
+        ) : (
+          <p>{item.description}</p>
+        )}
       </div>
     </div>
   );
@@ -39,22 +42,61 @@ function ExperienceNode({ item }: any) {
 
 function ExperienceSection({ section }: any) {
   const renderExperiences = () => {
-    return section.nodes.map((item: any, index: number) => {
-      return <ExperienceNode key={index} item={item} />;
-    });
+    if (section?.nodes) {
+      return section.nodes.map((item: any, index: number) => {
+        return <ExperienceNode key={index} item={item} />;
+      });
+    }
+    if (section?.description) {
+      return <p>{section.description}</p>;
+    }
   };
 
   return (
-    <div>
+    <div className="section">
       <Heading title={section.title} />
       {renderExperiences()}
     </div>
   );
 }
 
+function DateStamp({ title, date }: any) {
+  return (
+    <p>
+      {title} / <span className="date">{date}</span>
+    </p>
+  );
+}
+
+function Education({ section }: any) {
+  return (
+    <div className="section">
+      <Heading title={section.title} />
+      <DateStamp title={section.place} date={section.date} />
+      <p>{section.description}</p>
+    </div>
+  );
+}
+
+function Technologies({ section }: any) {
+  return (
+    <div className="section">
+      <Heading title={section.title} />
+      {section.nodes.map((item: any, index: number) => {
+        return (
+          <Fragment key={index}>
+            <h3>{item.type}</h3>
+            <List items={item.description} />
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
+
 function Resume() {
   return (
-    <div className="content">
+    <div className="resume">
       <div className="header">
         <h1>{resumeData.heading.name}</h1>
         <div className="header-socials">
@@ -76,10 +118,13 @@ function Resume() {
       </div>
       <div className="resume-body">
         {resumeData.body.map((section, index) => {
-          if (section?.nodes) {
-            return <ExperienceSection section={section} key={index} />;
+          if (section.type === "education") {
+            return <Education key={index} section={section} />;
           }
-          return <Section section={section} key={index} />;
+          if (section.type === "technologies") {
+            return <Technologies section={section} />;
+          }
+          return <ExperienceSection section={section} key={index} />;
         })}
       </div>
     </div>
